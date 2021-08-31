e9ee5dd5f8af3b2703fcb7923f16c8129721d995
natnew <natashanewboldlondon@gmail.com>
2021-08-31 19:41:47 +0100

    About Me Project
    About Me Project

---
+++ python/AboutMe.py
@@ -0,0 +1,39 @@
+print('Hello World!')
+
+print('Here is a picture of a dog:')
+print(' 0____')
+print(' ||||')
+
+print('My Favourite animals are cats:')
+print('This is a cat: ')
+print(' o____')
+print(' ||||')
+
+print('I live in a large city')
+print('This is my city:')
+print('__ __')
+print('| || |')
+print('| || |')
+print('| || |')
+print('| || |')
+print('__ __')
+
+print('In the year 2025, I will be:')
+print(2025 - 1991)
+print('years old')
+
+born = input('What year was you born? ')
+born = int(born)
+print(2025 - born)
+
+print('here is a wave:')
+print('/\/' * 10)
+
+print('This is a flower:')
+print('  00')
+print(' 0000')
+print('000000')
+print(' 0000')
+print('  00')
+
+
